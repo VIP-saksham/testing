fd3066de44469a3b14f655cd5cbb2c8e96eb6c87
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-12-08 01:48:41 +0530

    Update __init__.py

--- AviaxMusic/platforms/__init__.py
+++ AviaxMusic/platforms/__init__.py
@@ -1,7 +1,26 @@
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
+from AviaxMusic.core.bot import Aviax
+from AviaxMusic.core.dir import dirr
+from AviaxMusic.core.git import git
+from AviaxMusic.core.userbot import Userbot
+from AviaxMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+nand = Aviax()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
