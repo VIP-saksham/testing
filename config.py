55c5b7697a115c6ff31252f5b7837e0e715cc53e
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-12-08 00:29:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,6 +47,7 @@ GIT_TOKEN = getenv(
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THeDreamNetwork")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DREAMCHATZ")
+SUPPORT_GROUP = getenv("SUPPORT_CHAT", "https://t.me/DREAMCHATZ")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -123,6 +124,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
